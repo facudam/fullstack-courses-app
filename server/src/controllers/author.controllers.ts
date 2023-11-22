b70c105a1b2edb18c60e42b1918a18052cbdb315
facudam <facundo89caceres@gmail.com>
2023-11-21 22:02:57 -0300

    Adding update and create author controllers

--- server/src/controllers/author.controllers.ts
+++ server/src/controllers/author.controllers.ts
@@ -23,3 +23,26 @@ export const getAuthorById = async(req: Request, res: Response) => {
         return res.status(500).json(serverErrorMessage)
     }
 }
+
+export const createAuthor = async(req: Request, res: Response) => {
+    try {
+        const { name, country } = req.body;
+        pool.query('INSERT INTO author (author_name, author_country) VALUES (?,?)', [name, country]);
+        res.json({ author_name: name, author_country: country })
+    } catch(error) {
+        res.status(500).send(`Sorry, there's been an error: ${error}`)
+    }
+}
+
+export const updateAuthor = async(req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name, country } = req.body;
+        const [result] = await pool.query('UPDATE author SET author_name = IFNULL(?, author_name), author_country = IFNULL(?,author_country) WHERE author_id = ?', [name, country, id])
+
+        console.log(result)
+        res.send('Author successfully updated')
+    } catch(error) {
+        res.status(500).send(`Sorry, there's been an error: ${error}`)
+    }
+}
--- server/src/routes/authors.routes.ts
+++ server/src/routes/authors.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getAuthorById, getAuthors } from '../controllers/author.controllers';
+import { createAuthor, getAuthorById, getAuthors, updateAuthor } from '../controllers/author.controllers';
 
 const router = Router()
 
 router.get('/api/authors', getAuthors)
+router.post('/api/authors', createAuthor)
 
+router.patch('/api/authors/:id', updateAuthor)
 router.get('/api/authors/:id', getAuthorById)
 
 export default router;
