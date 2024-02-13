f398fb1930477b0f2b917dc78f0f3a0b16687149
facudam <facundo89caceres@gmail.com>
2024-02-13 19:43:08 -0300

    Add login validation

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,7 +1,10 @@
 import axios, { AxiosResponse } from "axios";
+import styles from './Login.module.css'
 import { ChangeEvent, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import apiBaseUrl from "../../services/api/endpoints/apiBaseUrl";
+import Eye from "../../components/eye/Eye";
+import { toggleEye } from "../../helpers/toggleEye";
 
 
 export const Login = () => {
@@ -9,6 +12,7 @@ export const Login = () => {
   const [ email, setEmail ] = useState<string>("");
   const [ password, setPassword ] = useState<string>("");
   const [ isDataInvalid, setIsDataInvalid ] = useState<boolean>(false);
+  const [ isDataEmpty, setIsDataEmpty ] = useState<boolean>(false)
 
   const handleEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
   const handlePassword = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
@@ -28,12 +32,19 @@ export const Login = () => {
 
   const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    axios
-      .post(
-        `${ apiBaseUrl }/api/login`,
-        { email, password },
-        { withCredentials: true }
-      )
+
+    const hasEmptyData = 
+      email.trim().length === 0 ||
+      password.trim().length === 0;
+
+    if (hasEmptyData) {
+      setIsDataEmpty(true)
+      return
+    }
+
+    setIsDataEmpty(false)
+
+    axios.post(`${ apiBaseUrl }/api/login`, { email, password }, { withCredentials: true })
       .then((res: AxiosResponse) => {
         if (res.data.login) {
           setIsDataInvalid(false);
@@ -50,7 +61,7 @@ export const Login = () => {
     <div>
       <form action="" onSubmit={  handleSubmit}>
         <div>
-          <label htmlFor="email">Email</label>
+          <label htmlFor="email">Email: </label>
           <input
             onChange={  handleEmail}
             type="email"
@@ -58,9 +69,13 @@ export const Login = () => {
             placeholder="Enter Email"
             value={email}
           />
+          {
+            (isDataEmpty && email.trim().length === 0) &&
+              <span>Ingrese su email</span>
+          }
         </div>
         <div>
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Contraseña: </label>
           <input
             onChange={ handlePassword }
             type="password"
@@ -68,10 +83,19 @@ export const Login = () => {
             placeholder="Enter password"
             value={ password }
           />
+          {
+            (isDataEmpty && password.trim().length === 0) &&
+              <span>Ingrese su contraseña</span>
+          }
+          <button
+              onClick={ (e) => toggleEye(e) } 
+              className={ styles.button }>
+              <Eye />
+          </button>
         </div>
-        <button type="submit">Log in</button>
-        <p>Don't you have an account already?</p>
-        <Link to="/signup">Create an account</Link>
+        <button type="submit">Inicia sesión</button>
+        <p>¿Todavia no estás registrado?</p>
+        <Link to="/signup">Regístrate</Link>
       </form>
       { isDataInvalid && <span>Usuario y/o contraseña inválidos</span> }
     </div>
