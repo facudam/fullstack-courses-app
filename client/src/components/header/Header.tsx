187641faae3bdad75a05c5db7e72ef01d1139aba
facudam <facundo89caceres@gmail.com>
2024-02-19 21:58:11 -0300

    Add tag keywords at Header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -12,6 +12,11 @@ export const Header: FC = () => {
         <header className={ styles.header }>
             <div className={ styles['header-ctn'] }>
                 <div className={ styles['header-ctn__titles'] }>
+                    <ul className={ styles.keywords }>
+                        <li>Explora</li>
+                        <li>Descubre</li>
+                        <li>Contribuye</li>
+                    </ul>
                     <h1>Tu formación al alcance de tu mano</h1>
                     <p>La biblioteca online con los mejores cursos de tecnología para Back-End, Front-End, Testing, Diseño UX/UI y más. Encuentra los cursos que necesitas para desarrollar tus habilidades IT, sin importar tu nivel de experiencia.</p>
                 </div>
