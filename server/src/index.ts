20bb2d7629b92e9d177ed6282d3f70fa7987d2dd
facudam <facundo89caceres@gmail.com>
2024-03-16 20:46:55 -0300

    Modify cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use(session({
 }))
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://courseslibra.vercel.app',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
 }))
