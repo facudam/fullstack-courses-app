b14823ea1430e7f7b27792daaffca02bff18ec96
facudam <facundo89caceres@gmail.com>
2024-04-21 19:53:39 -0300

    Add restOfStars at StarRating to complete 5 stars

--- client/src/components/starRating/StarRating.tsx
+++ client/src/components/starRating/StarRating.tsx
@@ -1,4 +1,4 @@
-import { halfStar, star } from "../../assets/images/images";
+import { emptyStar, halfStar, star } from "../../assets/images/images";
 import { FC } from "react";
 import styles from './StarRating.module.css'
 
@@ -9,18 +9,24 @@ interface Props {
 const StarRating: FC<Props> = ({ rating, rates_quantity }) => {
     const filledStars: number = Math.floor(rating);
     const hasHalfStar: boolean = rating % 1 !== 0;
+    const restOfStars: number = 5 - filledStars - (hasHalfStar ? 1 : 0);
 
     return(
         <div className={ styles['flex-ctn'] }>
             <p><strong>{ Number(rating).toFixed(1) }</strong></p>
             <div className={ styles['stars-ctn'] }>
                 {
-                    [...Array(filledStars)].map((_, index) => (
+                    [...Array(filledStars)].map((_ , index) => (
                         <img width={ 16 } src={ star } key={ index } alt="estrella"  />
                     ))
                 }
                 {
                     hasHalfStar && <img width={ 16 } src={ halfStar } alt="media estrella" />
+                }
+                {
+                    [...Array(restOfStars)].map((_ , index) => (
+                        <img width={ 16 } src={ emptyStar } key={ index } alt="estrella"  />
+                    ))
                 }
                 
             </div>
